f2d524401bbdb00ae8e7d66083fe646bcc8a68da
Stephen <stevriss22@gmail.com>
2021-11-03 10:47:55 -0400

    commit tree feb21963ce8388a638d6b3e1c9b4aa8622f75264

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+    @@items = []
+    attr_accessor :total
 
+    def intialize(total == 0, discount)
+        @total = total
+        @discount = discount
+        @@items=items
+
+
+        @@items << self
+    end
+
+    def discount
+
+    end
+
+    def self.items
+        @@items
+    end
+
+    
+    
+end
